28d06ee36d19e05ea094c09bc384e5cc63c39d2f
Patryk Makowski <patryk.makowski23@gmail.com>
2023-01-09 20:11:55 +0100

    Small adjustments

--- emails/footer.tsx
+++ emails/footer.tsx
@@ -1,100 +1,125 @@
-import React from 'react';
-import { Section } from '@react-email/section';
-import { Column } from '@react-email/column';
-import { Html } from '@react-email/html';
-import { Text } from '@react-email/text';
-import { Container } from '@react-email/container';
-import { Img } from '@react-email/img';
-import { Link } from '@react-email/link';
-import logo from "./assets/logo2.svg"
+import React from "react";
+import { Section } from "@react-email/section";
+import { Text } from "@react-email/text";
+import { Container } from "@react-email/container";
+import { Img } from "@react-email/img";
+import { Link } from "@react-email/link";
 
 export type formProps = {
   name: string;
   position: string;
-  phoneNumber:string;
-  email:string;
-}
+  phoneNumber: string;
+  email: string;
+};
 
-
-const Footer: React.FC<formProps > = ({ name, position, phoneNumber, email}): JSX.Element => {
+const Footer: React.FC<formProps> = ({
+  name,
+  position,
+  phoneNumber,
+  email,
+}): JSX.Element => {
   return (
-        <Container style={mainContainer}>
-            <Section style={mainSection} align="left">
-                  <Img src='https://samorzad.p.lodz.pl/addons/shared_addons/themes/adonis/img/logo-brown.png' alt="logo" style={imageStyle}/>
-                  <Container style={rightColumn}>
-                      <Text style={nameStyle}>{name}</Text>
-                      <Text style={positionStyle}>{position}</Text>
-                      <Text style={phoneNumberStyle}>tel. +48{phoneNumber} <Link href="https://example.com"> {email} </Link></Text>
-                      <Container>
-                      <Text>Samorząd Studencki Politechniki Łódźkiej </Text>
-                      <Text>al. Politechniki 3a | 90-924 Łódź <br/></Text>
-                      <Text>tel. +48 42 631 28 41 <Link href="https://example.com">www.samorzad.p.lodz.pl</Link>
-                      </Text>
-                      </Container>
-                  </Container>
-            </Section>
-            <Container style={absoulute}>
-          <Text style={bottomContentStyle}> Treść tej wiadomości zawiera informacje przeznaczone tylko dla adresata. Jeżeli nie jesteście Państwo jej adresatem, bądź otrzymaliście ją przez pomyłkę, prosimy o powiadomienie o tym nadawcy oraz trwałe jej usunięcie.</Text>
-          <Text style={bottomContentStyle}>This email contains information intended solely for the use of the individual to whom it is addressed. If you are not the intended recipient or if you have received this message in error, please notify the sender and delete it from your system.</Text>
+    <Container style={mainContainer}>
+      <Section style={mainSection} align="left">
+        <Img
+          src="https://samorzad.p.lodz.pl/addons/shared_addons/themes/adonis/img/logo-brown.png"
+          alt="logo"
+          style={imageStyle}
+        />
+        <Container style={rightColumn}>
+          <Text style={nameStyle}>{name}</Text>
+          <Text style={positionStyle}>{position}</Text>
+          <Text style={phoneNumberStyle}>
+            tel. +48{phoneNumber}{" "}
+            <Link href="https://example.com"> {email} </Link>
+          </Text>
+          <Container>
+            <Text>Samorząd Studencki Politechniki Łódźkiej </Text>
+            <Text>
+              al. Politechniki 3a | 90-924 Łódź <br />
+            </Text>
+            <Text>
+              tel. +48 42 631 28 41{" "}
+              <Link href="https://example.com">www.samorzad.p.lodz.pl</Link>
+            </Text>
+          </Container>
         </Container>
-        </Container>
+      </Section>
+      <Container style={absoulute}>
+        <Text style={bottomContentStyle}>
+          {" "}
+          Treść tej wiadomości zawiera informacje przeznaczone tylko dla
+          adresata. Jeżeli nie jesteście Państwo jej adresatem, bądź
+          otrzymaliście ją przez pomyłkę, prosimy o powiadomienie o tym nadawcy
+          oraz trwałe jej usunięcie.
+        </Text>
+        <Text style={bottomContentStyle}>
+          This email contains information intended solely for the use of the
+          individual to whom it is addressed. If you are not the intended
+          recipient or if you have received this message in error, please notify
+          the sender and delete it from your system.
+        </Text>
+      </Container>
+    </Container>
   );
 };
 
 const mainContainer = {
-    width:800,
-    height:450,
-    backgroundColor: '#ffffff',
-    border:"solid 1px black",
-    //Jest ustawiony jakiś dziwny maxwidth. JaK dla mnie lepiej to usunąć
-    maxWidth: "100vw",
-    position:"relative"
-}
+  width: 800,
+  height: 450,
+  backgroundColor: "#ffffff",
+  border: "solid 1px black",
+  //Jest ustawiony jakiś dziwny maxwidth. JaK dla mnie lepiej to usunąć
+  maxWidth: "100vw",
+  position: "relative",
+} as const;
+
 const mainSection = {
-    marginLeft:30,
-    marginTop:20,
-    width:700,
-    height:200,
-    display:"flex"
-  };
+  marginLeft: 30,
+  marginTop: 20,
+  width: 700,
+  height: 200,
+  display: "flex",
+} as const;
 
 const imageStyle = {
-  width:300,
-}
+  width: 300,
+} as const;
 
 const absoulute = {
-  position:"absolute",
-  top:250,
-  left:30,
-  width:800,
-}
+  position: "absolute",
+  top: 250,
+  left: 30,
+  width: 800,
+} as const;
 
 const rightColumn = {
-  marginLeft:20,
-}
+  marginLeft: 20,
+} as const;
+
 const nameStyle = {
-  marginBlockStart:0,
-  color:"#000000",
-  fontSize:20,
-}
+  marginBlockStart: 0,
+  color: "#000000",
+  fontSize: 20,
+} as const;
 
 const positionStyle = {
-  marginBlockStart:0,
-  color:"#000000",
-  fontSize:10,
-}
+  marginBlockStart: 0,
+  color: "#000000",
+  fontSize: 10,
+} as const;
 
 const phoneNumberStyle = {
-  marginBlockStart:0,
-  color:"#000000",
-  fontSize:10,
-}
+  marginBlockStart: 0,
+  color: "#000000",
+  fontSize: 10,
+} as const;
 
 const bottomContentStyle = {
-  color:"#000000",
-  fontSize:12,
-  marginTop:30,
-  width:750,
-}
+  color: "#000000",
+  fontSize: 12,
+  marginTop: 30,
+  width: 750,
+} as const;
 
 export default Footer;
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,134 +1,159 @@
-import { TextInput, Checkbox, Button, Group, Box, Radio } from '@mantine/core';
-import { useForm } from '@mantine/form';
-import { useState } from 'react';
-import Footer, {formProps} from '../emails/footer';
-import { render } from '@react-email/render';
-import './App.css';
+import { TextInput, Checkbox, Button, Group, Box, Radio } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from "react";
+import Footer, { formProps } from "../emails/footer";
+import { render } from "@react-email/render";
+import "./App.css";
 
 export default function Form() {
-
   const [showFooter, setShowFooter] = useState<boolean>(false);
-  const [inputValues, setInputValues] = useState<formProps>({name:"", position:"", phoneNumber:"",email:""});
+  const [inputValues, setInputValues] = useState<formProps>({
+    name: "",
+    position: "",
+    phoneNumber: "",
+    email: "",
+  });
   const [wrsMember, setWrsMember] = useState(false);
   const [wrsName, setWrsName] = useState("");
 
   const form = useForm({
     initialValues: {
-      name:'',
-      position:'',
-      email: '',
-      phoneNumber:'',
+      name: "",
+      position: "",
+      email: "",
+      phoneNumber: "",
     },
 
     validate: {
-        //można zmienić, żeby regex sprawdzał całe .samorzad.p.lodz.pl. Inne errory też pewnie będą do zmiany(Przede wszystkim wymagania długości)
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Nieprawidłowy mail'),
-      name: (value) => value.length > 4 ? null : 'Masz tak krótkie imię i nazwisko?',
-      position: (value) => value.length > 4 ? null : 'Nie ma stanowiska o tak krótkiej nazwie',
-      phoneNumber: (value) => value.length ===9 ? null : "Coś jest nie tak z twoim numerem telefonu" 
-
+      //można zmienić, żeby regex sprawdzał całe .samorzad.p.lodz.pl. Inne errory też pewnie będą do zmiany(Przede wszystkim wymagania długości)
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Nieprawidłowy mail"),
+      name: (value) =>
+        value.length > 4 ? null : "Masz tak krótkie imię i nazwisko?",
+      position: (value) =>
+        value.length > 4 ? null : "Nie ma stanowiska o tak krótkiej nazwie",
+      phoneNumber: (value) =>
+        value.length === 9 ? null : "Coś jest nie tak z twoim numerem telefonu",
     },
   });
 
   return (
     <div>
-    <Box className='formContainer' sx={{ 
-        // Trzeba jakoś dać forularz na środek, pewnie są lepsze opcje na to. 
-        //Teraz zakomentowane, żeby dobrze było widać stopkę
-        marginTop:200,
-        maxWidth: 300,
-        minHeight:390,
-        marginBottom:50,
-        padding:10,
-        borderRadius:5,
-         }} mx="auto">
-      <form onSubmit={form.onSubmit((values) => {
-        //zmienna showFooter i setter do niej jest potrzebny do pokazania rezultatu. Potem do wywalenia
-        setShowFooter(true);
-        const footerValues:formProps = {
-            name: values.name,
-            position: values.position,
-            phoneNumber: values.phoneNumber,
-            email: values.email
-        }
-        console.log(footerValues);
-        setInputValues(footerValues); 
-        //Tu jest html potrzebny do wygenerowania stopki. Na razie jest tylko wyświetlany w konsoli. W przyszłości trzeba go jakoś przesłąć do gmaila
-        const html = render(<Footer name={inputValues.name} position={inputValues.position} phoneNumber={inputValues.phoneNumber} email={inputValues.email}> </Footer>);
-        console.log(html);
-    })}>
-        {/* Przed Inputami jakiś tytuł trzeba dać i może wyjaśnienie co robi aplikacja */}
-        <TextInput
-          withAsterisk
-          label="Imię i nazwisko"
-          placeholder="Imię i nazwisko"
-          {...form.getInputProps('name')}
-          radius={6}
-          sx={{
-            marginTop:15,
-            marginBottom:10,
-          }}
-        />
-
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="your@email.com"
-          {...form.getInputProps('email')}
-          radius={6}
-          sx={{
-            marginBottom:10,
-          }}
-        />
-
-        <TextInput
-          withAsterisk
-          label="Numer Telefonu"
-          placeholder="Numer Telefonu"
-          {...form.getInputProps('phoneNumber')}
-          radius={6}
-          sx={{
-            marginBottom:10,
-          }}
-        />
-
-        <TextInput
-          withAsterisk
-          label="Stanowisko"
-          placeholder="Stanowisko"
-          {...form.getInputProps('position')}
-          radius={6}
-          sx={{
-            marginBottom:10,
-          }}
-        />  
-        <Checkbox label="Jestem członkiem WRS-u" checked={wrsMember} onChange={(event) => {
-            setWrsMember(event.currentTarget.checked);
-            setWrsName("");
-            }} 
+      <Box
+        className="formContainer"
+        sx={{
+          // Trzeba jakoś dać forularz na środek, pewnie są lepsze opcje na to.
+          //Teraz zakomentowane, żeby dobrze było widać stopkę
+          marginTop: 200,
+          maxWidth: 300,
+          minHeight: 390,
+          marginBottom: 50,
+          padding: 10,
+          borderRadius: 5,
+        }}
+        mx="auto"
+      >
+        <form
+          onSubmit={form.onSubmit((values) => {
+            //zmienna showFooter i setter do niej jest potrzebny do pokazania rezultatu. Potem do wywalenia
+            setShowFooter(true);
+            const footerValues: formProps = {
+              name: values.name,
+              position: values.position,
+              phoneNumber: values.phoneNumber,
+              email: values.email,
+            };
+            console.log(footerValues);
+            setInputValues(footerValues);
+            //Tu jest html potrzebny do wygenerowania stopki. Na razie jest tylko wyświetlany w konsoli. W przyszłości trzeba go jakoś przesłąć do gmaila
+            const html = render(
+              <Footer
+                name={inputValues.name}
+                position={inputValues.position}
+                phoneNumber={inputValues.phoneNumber}
+                email={inputValues.email}
+              />
+            );
+            console.log(html);
+          })}
+        >
+          {/* Przed Inputami jakiś tytuł trzeba dać i może wyjaśnienie co robi aplikacja */}
+          <TextInput
+            withAsterisk
+            label="Imię i nazwisko"
+            placeholder="Imię i nazwisko"
+            {...form.getInputProps("name")}
+            radius={6}
             sx={{
-              marginBottom:10,
+              marginTop: 15,
+              marginBottom: 10,
             }}
-            />
-        {wrsMember && <Radio.Group
-            value={wrsName}
-            onChange={setWrsName}
-            name="WrsChoice"
-            label="Wybierz swój WRS"
-            description="Możesz wybrać tylko jeden"
-            >       
-            <Radio value="WEEIA" label="WEEIA" />
-            <Radio value="WTIMS" label="WTIMS" />
-            <Radio value="OiZ" label="OiZ" />
-            </Radio.Group>}
+          />
 
-        <Group position="right" mt="md">
-          <Button sx={{backgroundColor: "#673ab7"}} type="submit">Submit</Button>
-        </Group>
-      </form>
-    </Box>
-    {showFooter && <Footer name={inputValues.name} position={inputValues.position} phoneNumber={inputValues.phoneNumber} email={inputValues.email}></Footer>}
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="your@email.com"
+            {...form.getInputProps("email")}
+            radius={6}
+            sx={{
+              marginBottom: 10,
+            }}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Numer Telefonu"
+            placeholder="Numer Telefonu"
+            {...form.getInputProps("phoneNumber")}
+            radius={6}
+            sx={{
+              marginBottom: 10,
+            }}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Stanowisko"
+            placeholder="Stanowisko"
+            {...form.getInputProps("position")}
+            radius={6}
+            sx={{
+              marginBottom: 10,
+            }}
+          />
+          <Checkbox
+            label="Jestem członkiem WRS-u"
+            checked={wrsMember}
+            onChange={(event) => {
+              setWrsMember(event.currentTarget.checked);
+              setWrsName("");
+            }}
+            sx={{
+              marginBottom: 10,
+            }}
+          />
+          {wrsMember && (
+            <Radio.Group
+              value={wrsName}
+              onChange={setWrsName}
+              name="WrsChoice"
+              label="Wybierz swój WRS"
+              description="Możesz wybrać tylko jeden"
+            >
+              <Radio value="WEEIA" label="WEEIA" />
+              <Radio value="WTIMS" label="WTIMS" />
+              <Radio value="OiZ" label="OiZ" />
+            </Radio.Group>
+          )}
+
+          <Group position="right" mt="md">
+            <Button sx={{ backgroundColor: "#673ab7" }} type="submit">
+              Submit
+            </Button>
+          </Group>
+        </form>
+      </Box>
+      {showFooter && <Footer {...inputValues} />}
     </div>
   );
 }
-  
